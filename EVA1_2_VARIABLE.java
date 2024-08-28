2d5c2959ba54360f078e8fe0a52e68497a3fa8df
Payala1100 <joseantoniopepe772@gmail.com>
2024-08-28 17:21:09 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLE.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this teplate file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variable;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_VARIABLE {
+
+    /**
+     * @param args the command line arguments
+     */                                         
+    public static void main(String[] args) {        //TIPOS DE DATOS
+        System.out.println("HOla Mundo!1"); //CAdena de texto
+        System.out.println(100); //Entero
+        System.out.println(3.1416); //Numeros flotantes
+        System.out.println("$"); //Caracter
+        System.out.println(true); ///Valores Logicos (Booleanoos)
+        System.out.println(false); ///Valores Logicos (Booleanoos)
+       // El punto y coma sdeclara que acabste la secuecnia  
+    
+    String Nombre;
+    String Apellido;
+    String Curp;
+    double Promedio;
+    int edad;
+    
+    edad = 18;
+    
+    Nombre = "Jose Antonio";
+    Apellido = "Ayala Valois";
+    Promedio = 7.4;
+    
+    System.out.println(edad);
+    System.out.println(Nombre);
+    System.out.println(Apellido);
+    System.out.println(Promedio);
+    
+    
+    
+    
+    }
+
+    //Enertos
+    //int
+    //long
+    //short
+    
+    //Numeros Cion Parte Flotante
+    //float
+    //double
+    
+    //Cadenas de Texto
+    //strinf
+    
+    //Caracteres
+    //Char
+    
+    //Valores Logico
+    //cooelean
+    
+    //Decalracion de Variables
+    //Sintaxis Para decalrar Variables  
+    //Tipo de Dato Nombre De La Variable
+    
+
+    
+    
+}
---
+++ EVA1_3_VARIABLES2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_3_variables2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Marca
+        //Modelo
+        //Año
+        //Kilometraje
+        //No.Cilindros
+        
+        String Marca;
+        String Modelo;
+        int  Año;
+        double Precio;
+
+        //Asignacio
+        
+        Marca = "Mazda";
+        Modelo = "Linux";
+        Año = 2021;
+        Precio = 200231.31;
+        
+        System.out.println(Marca);
+        System.out.println(Modelo);      
+        System.out.println(Año);
+        System.out.println(Precio);
+        
+        
+        
+    }
+    
+}
